0ff0ba1a7d1fb698efdd52ab78f89ba0d1cf9daa
VinitMPatel <vinit.patel@etatvasoft.com>
2024-01-16 18:32:01 +0530

    fifth day completed

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
 const sidebarbtn = document.getElementById("sidebar-btn");
 const sidebarcontent = document.getElementById("sidebar-content");
 const maincontent = document.getElementById("main-content");
+var flag = 0;
 sidebarbtn.addEventListener("click",function(){
-    sidebarcontent.style.display = "none";
-    maincontent.classList.replace('col-10','col-12')
+    if(flag==0){
+        sidebarcontent.style.display = "none";
+        maincontent.classList.replace('col-md-10','col-md-12');
+        flag = 1;
+    }
+    else{
+        sidebarcontent.style.display = "flex";
+        maincontent.classList.replace('col-md-12','col-md-10');
+        flag = 0;
+    }
 })
